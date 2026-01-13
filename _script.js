e2680a2610fc6cc9c59e4277a1210d814517ceee
christenally <13616443+Christenally@users.noreply.github.com>
2026-01-12 23:28:06 -0600

    Working on responsiveness

--- _script.js
+++ _script.js
@@ -2,8 +2,9 @@
 const navLinksDiv = document.querySelector(".nav-links");
 const navButton = document.getElementById("nav-button");
 const hamburgerContainer = document.querySelector(".hamburger-container");
+const heroImage = document.getElementById("hero-img");
 
-const tablet = 768;
+const tablet = 769;
 const mobile = 375;
 
 //* Event Listeners
@@ -19,9 +20,15 @@ function tabletAdjust() {
     navLinksDiv.classList.add("hide-me");
     navButton.classList.add("hide-me");
     hamburgerContainer.classList.remove("hide-me");
-    return null;
+    //!Hero
+    heroImage.src = "/assets/images/image-home-hero-small.webp";
+    return;
   }
   navLinksDiv.classList.remove("hide-me");
   navButton.classList.remove("hide-me");
   hamburgerContainer.classList.add("hide-me");
+  //!Hero
+  heroImage.src = "/assets/images/image-home-hero-large.webp";
 }
+
+//!remeber to do onload

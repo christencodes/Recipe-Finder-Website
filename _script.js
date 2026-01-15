5593a500e546a1525dcbd03520c36ee4300915e7
christenally <13616443+Christenally@users.noreply.github.com>
2026-01-14 23:32:45 -0600

    Working on recipe page

--- _script.js
+++ _script.js
@@ -4,6 +4,28 @@ const navButton = document.getElementById("nav-button");
 const hamburgerContainer = document.querySelector(".hamburger-container");
 const heroImage = document.getElementById("hero-img");
 
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+const maxPrepTime = document.getElementById("max-prep-time");
+const maxPrepTimeOptions = document.querySelector(".max-prep-time-options");
+
+maxPrepTime.addEventListener("click", (e) => {
+  console.log("hello");
+  maxPrepTimeOptions.classList.toggle("hide-me");
+});
+
+const clear = document.getElementById("clear");
+
+clear.addEventListener("click", (e) => {
+  clearChecked();
+});
+
+function clearChecked() {
+  radioButtons.forEach((e) => {
+    e.checked = false;
+  });
+}
+
 const tablet = 769;
 const mobile = 375;
 
@@ -31,4 +53,17 @@ function tabletAdjust() {
   heroImage.src = "/assets/images/image-home-hero-large.webp";
 }
 
-//!remeber to do onload
+//!remember to do onload
+
+//function to get the json data
+
+//populate the cards with the data
+
+/* ?
+1. Image - based on the screen width
+2. Title - (not the slug)
+3. Overview
+4. Servings
+5. Prep Minutes
+6. Cook Minutes
+4. */
